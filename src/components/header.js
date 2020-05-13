c1f57fcc7ad93e8a0758a3223c4b73cc0f37354d
JacobYoung68 <68jacobyoung@gmail.com>
2020-05-13 18:13:55 +0100

    Added major styling changes

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,24 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "../components/Style.css"
+
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
+    style={
+      {
+        //marginBottom: `1.45rem`,
+      }
+    }
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: "95%",
-        padding: `1.45rem 1.0875rem`,
+        //padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `White`,
-            textDecoration: `none`,
-            fontSize: "72px",
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <h1 id="jyTitle">{siteTitle}</h1>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,19 +7,36 @@ import "../components/Style.css"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Projects.</h1>
+    <h1 id="subheading">Projects.</h1>
+
     <div id="Posts">
-      <div id="Post">
-        <h1>Hi.</h1>
-      </div>
-      <div id="Post"></div>
-      <div id="Post"></div>
-      <div id="Post"></div>
-      <div id="Post"></div>
-      <div id="Post"></div>
-      <div id="Post"></div>
-      <div id="Post"></div>
+      <button className="Post">
+        <img src={require("../images/Testimg1.jpg")} alt="Mobile Morse Code" />
+        <h1 id="Post1">
+          Mobile <br />
+          Morse Code.
+        </h1>
+      </button>
+
+      <button className="Post">
+        <img src={require("../images/Testimg2.jpg")} alt="Test 1" />
+        <h1 id="Post1">Test 1.</h1>
+      </button>
+
+      <button className="Post">
+        <img src={require("../images/Testimg2.jpg")} alt="Test 2" />
+        <h1 id="Post1">Test 2.</h1>
+      </button>
+
+      <button className="Post">
+        <img src={require("../images/Testimg1.jpg")} alt="Test 3" />
+        <h1 id="Post1">Test 3.</h1>
+      </button>
     </div>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, minimum-scale=1"
+    />
   </Layout>
 )
 
